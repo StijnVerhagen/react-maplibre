d6add39eeab5ffda6f92a6fb8861c4bb4f4fca3c
Stijn Verhagen <stijn@moonlysoftware.com>
2023-02-13 14:33:46 +0100

    Initial POC commit

--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -1,46 +1,61 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
+import Map, { NavigationControl } from 'react-map-gl';
+import MapLibreGl from 'maplibre-gl';
 
-// External
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import '@mapbox/mapbox-gl-draw/dist/mapbox-gl-draw.css';
+import DrawControl from './draw-control';
 
-// Components
-import { ReactComponent as Logo } from '../../assets/images/logo.svg';
+const Welcome = () => {
+  const [features, setFeatures] = useState({});
 
-const Welcome = () => (
-  <div className="flex h-screen w-full flex-col items-center justify-center text-center text-dark">
-    <div className="mb-6" data-aos="fade-up">
-      <Logo className="pointer-events-none h-24 w-24	motion-safe:animate-spin-slow" />
+  const onUpdate = useCallback((e) => {
+    setFeatures((currFeatures) => {
+      const newFeatures = { ...currFeatures };
+      for (const f of e.features) {
+        newFeatures[f.id] = f;
+      }
+      return newFeatures;
+    });
+  }, []);
+
+  const onDelete = useCallback((e) => {
+    setFeatures((currFeatures) => {
+      const newFeatures = { ...currFeatures };
+      for (const f of e.features) {
+        delete newFeatures[f.id];
+      }
+      return newFeatures;
+    });
+  }, []);
+  return (
+    <div className="flex h-screen w-full flex-col items-center justify-center text-center text-dark">
+      <Map
+        mapLib={MapLibreGl}
+        initialViewState={{
+          longitude: 16.62662018,
+          latitude: 49.2125578,
+          zoom: 14,
+        }}
+        style={{ width: '100%', height: ' 90vh' }}
+        mapStyle="https://api.maptiler.com/maps/streets/style.json?key=4g2o5EcoGa5b3sc9qbM8"
+      >
+        <NavigationControl position="top-left" />
+        <DrawControl
+          position="top-left"
+          displayControlsDefault={true}
+          controls={{
+            polygon: true,
+            trash: true,
+          }}
+          defaultMode="draw_polygon"
+          onCreate={onUpdate}
+          onUpdate={onUpdate}
+          onDelete={onDelete}
+        />
+      </Map>
+      <p>={Object.values(features)[0].geometry.coordinates.toString()}</p>
     </div>
-    <h3 className="mb-6" data-aos="fade-up" data-aos-delay="100">
-      Edit &zwnj;
-      <code className="inline-block max-w-full whitespace-normal break-all">src/components/Welcome/Welcome.jsx</code>
-      &zwnj; and save to reload.
-    </h3>
-    <a
-      className="text-dark transition hover:text-black"
-      href="https://reactjs.org"
-      target="_blank"
-      rel="noopener noreferrer"
-      data-aos="fade-up"
-      data-aos-delay="200"
-    >
-      <h4>
-        Learn React &nbsp; <FontAwesomeIcon icon="fa-solid fa-arrow-right" className="text-base" />
-      </h4>
-    </a>
-    <a
-      className="text-dark transition hover:text-black"
-      href="https://github.com/jarntz/react"
-      target="_blank"
-      rel="noopener noreferrer"
-      data-aos="fade-up"
-      data-aos-delay="300"
-    >
-      <h4>
-        GitHub &nbsp; <FontAwesomeIcon icon="fa-brands fa-github" className="text-base" />
-      </h4>
-    </a>
-  </div>
-);
+  );
+};
 
 export default Welcome;
